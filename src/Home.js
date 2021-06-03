3ccb38bcf3687217d24f26602671ef5adbf6c394
99Atharva <atharvatilvankar@gmai.com>
2021-06-03 23:32:40 +0530

    New

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,59 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Home.css";
+import img1 from "./Elements/Logo.png";
+import img2 from "./Elements/time.svg";
+import img3 from "./Elements/comm.svg";
+import img4 from "./Elements/play.svg";
+
+
+
 
 
 function Home() {
-    const [count, setCount] = useState(0);
-    
-    const handleIncr =()=>{
-        setCount(count => count +1);
+
+    const Magnify =  {
+        
     }
 
-    const handleDelete = ()=> {
-        setCount(count => count - 1);
-    }
-    
     return (
         <div className="contain ">
-    
-            <button className="btn" onClick={handleDelete} >
-                -
-            </button>
-            <div className="counter_box la">
-               <h2>{count}</h2> 
+       <div className="1st_half">
+       <div className="logo">
+            <img src={img1}/> <h4>COLEARN</h4>
+            <div className="nav">
+                <ul >
+                <li>Product</li>
+                <li>Pricing</li>
+                <li>Contacts</li>
+                <li>Sigin</li>
+                </ul>
             </div>
-            <button className="btn" onClick={handleIncr}>
-                +
-            </button>
-            <div className="reset">
-                <button className="btn" onClick={()=>setCount(0)}>
-                    Reset
-                </button>
+        </div>
+        <div className="left_box">
+        <p className="head">Efficient learning,<br/>
+        made simpler.</p>
+
+        <p className="text">Find box a little too small and curl up with fur hanging out mew mew i just saw other cats inside the house and nobody ask me before using my litter box. Scratch me there, elevator butt eat my own ears throwup on your pillow paw at Intently sniff hand bleghbleghvomit my furball really tie the</p>
+        <div className="btn_grp ">
+            <button>Get Started</button>
+            <button id="btn2"> <img src={img4}/>See how it works</button>
+        </div>
+
+        <div className="last_container_left">
+            <div className="box">
+            <img className="icon" src={img2}/>
+            <p>Find box a little too small and curl fur hanging out mew mew </p>
             </div>
+            <div className="box">
+                
+               <img className="icon" src={img3} /> 
+               <p>Find box a little too small and curl fur hanging out mew mew </p>
+            </div>
+        </div>
+        </div>
+        
+       </div>
+        
         </div>
     )
 }
